55f030429af5ff3d61b66d19f79ac9f074b62042
jeroenvangrootel <vangrooteljeroen@gmail.com>
2017-05-15 13:20:04 +0200

    naam toegevoegd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'))
 app.get('/', function(req, res) {
   res.render("index", {
-    naam: "Wereld"
+    naam: "Jeroen"
   });
 });
 app.listen(app.get('port'), function() {
